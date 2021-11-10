a5cd13d0d73a0002647bd769b5f49f6e24e8e3f1
Hilaire Vangmbono Daliwa <hilaire.daliwa@odc.local>
2021-11-10 17:38:11 +0000

    appli tiktok

---
+++ lib/constant/data_json.dart
@@ -0,0 +1,41 @@
+List items = [
+  {
+    "videoUrl": "assets/videos/video_1.MP4",
+    "name": "Hilaire Daliwa🦋",
+    "caption": "Bonjour, tous le monde!!",
+    "songName": "bonne humeur - pour tous chez soi",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/1663771856684033.jpeg",
+    "likes": "1.5M",
+    "comments": "18.9K",
+    "shares": "80K",
+    "albumImg":
+        "https://images.unsplash.com/photo-1502982899975-893c9cf39028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    "videoUrl": "assets/videos/video_2.MP4",
+    "name": "Hilaire Daliwa",
+    "caption": "Oops 🙊 #fyp #cambodiatiktok",
+    "songName": "original sound - 💛💛Khantana 🌟",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/ba13e655825553a46b1913705e3a8617.jpeg",
+    "likes": "4.4K",
+    "comments": "5.2K",
+    "shares": "100",
+    "albumImg":
+        "https://images.unsplash.com/photo-1462804512123-465343c607ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+  },
+  {
+    "videoUrl": "assets/videos/video_3.mov",
+    "name": "Hilaire Zaflex",
+    "caption": "#블루모드",
+    "songName": "original sound - 🖤Khün MÄk🇰🇭",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/1664576339652610.jpeg",
+    "likes": "100K",
+    "comments": "10K",
+    "shares": "8.5K",
+    "albumImg":
+        "https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+];
